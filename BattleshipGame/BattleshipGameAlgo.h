8d135551c32705761d41d18289581857eaaba9b8
ItaysSofer <sofer.itay@gmail.com>
2017-04-01 17:45:19 +0300

    added global board size + implemented BattleshipGameAlgo + implemented StringUtils

--- BattleshipGame/BattleshipGameAlgo.h
+++ BattleshipGame/BattleshipGameAlgo.h
@@ -2,19 +2,23 @@
 #define BattleshipGameAlgoH
 
 #include "IBattleshipGameAlgo.h"
-
-enum class PlayerIndex {
-	PlayerA, PlayerB
-};
+#include <fstream>
+#include <vector>
 
 class BattleshipGameAlgo: public IBattleshipGameAlgo {
+	std::ifstream attackFile;
+
 public:
-	BattleshipGameAlgo(PlayerIndex playerIndex); //initiates player (path to attack file? already initiates attack file itself?)
-	~BattleshipGameAlgo() = default;
+	BattleshipGameAlgo(std::string path, std::string attackFileSuffix); //initiates player (path to attack file? already initiates attack file itself?)
+	~BattleshipGameAlgo() override;
 
 	void setBoard(const char** board, int numRows, int numCols) override; // called once to notify player on his board
 	std::pair<int, int> attack() override; // ask player for his move
 	void notifyOnAttackResult(int player, int row, int col, AttackResult result) override; // notify on last move result
+
+private:
+	std::string getCompleteAttackFilePath(std::string path, std::string fileSuffix);
+	bool isValidAttackMove(std::vector<std::string> pos);
 };
 
 #endif
