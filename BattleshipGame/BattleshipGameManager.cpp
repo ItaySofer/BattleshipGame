385c90000b20315d03a538a9e6d3842428ea0d82
Omer Sofer <omer.sofer@intel.com>
2017-04-03 18:45:46 +0300

    board validation modified

--- BattleshipGame/BattleshipGameManager.cpp
+++ BattleshipGame/BattleshipGameManager.cpp
@@ -10,7 +10,7 @@ void playGame(){
 };
 
 void BattleshipGameManager::readBoardFileToMatrix(const std::string boardFile, BattleBoard& gameBoard){
-	//cout << "Reading battleBoard from file: simpleBattleBoard.txt into class BattleBoard" << endl;
+
 	std::ifstream fin(boardFile);
 	gameBoard.R = RAW;
 	gameBoard.C = COL;
@@ -25,108 +25,198 @@ void BattleshipGameManager::readBoardFileToMatrix(const std::string boardFile, B
 void BattleshipGameManager::validateBoard(const BattleBoard& gameBoard){
 	int Acount = 0;
 	int Bcount = 0;
+	bool isValidRight;
+	bool isValidBottom;
 
 	//Search for valid ships
 	for (int i = 0; i < gameBoard.R; ++i){
 		for (int j = 0; j < gameBoard.C; ++j){
 			char* pos = std::find(std::begin(typeArr), std::end(typeArr), gameBoard.matrix[i][j]);	
 			if (pos != std::end(typeArr)) { //element found
-				if (((i == 0) || (gameBoard.matrix[i - 1][j] != gameBoard.matrix[i][j])) && 
-					((j == 0) || (gameBoard.matrix[i][j-1] != gameBoard.matrix[i][j]))){//if both left and top != gameBoard.matrix[i][j]
-					if (isValidShip(gameBoard.matrix[i][j], i, j, gameBoard)){
+				isValidRight = true;
+				isValidBottom = true;
+				//check if need to check ship to the right
+				if ((i == 0) || (gameBoard.matrix[i - 1][j] != gameBoard.matrix[i][j])){
+					//Check to the right
+					isValidRight = isValidShipRight(i, j, gameBoard);
+					if (isValidRight){
 						(std::distance(typeArr, pos) < NUM_OF_SHIP_TYPES) ? Acount++ : Bcount++;
 					}
+				}
+				//check if need to check ship to the bottom
+				if ((j == 0) || (gameBoard.matrix[i][j - 1] != gameBoard.matrix[i][j])){
+					//Check to the bottom
+					isValidBottom = isValidShipBottom(i, j, gameBoard);
+					if (isValidBottom){
+						(std::distance(typeArr, pos) < NUM_OF_SHIP_TYPES) ? Acount++ : Bcount++;
+					}
+				}
+				if (!isValidRight && !isValidBottom){
+					updateErrMsgArrWrongSize(gameBoard.matrix[i][j]);
 				}
 			}
 		}		
 	}
-	//TODO: check for adjacent
-	//TODO: go through errMsgArr array and print error messages if needed
+	//Check valid number of ships
+	if (Acount < VALID_SHIP_NUM){
+		errMsgArr[int(ErrorMsg::TOO_FEW_A)].first = true;
+	}
+	else if (Acount > VALID_SHIP_NUM){
+		errMsgArr[int(ErrorMsg::TOO_MANY_A)].first = true;
+	}
+
+	if (Bcount < VALID_SHIP_NUM){
+		errMsgArr[int(ErrorMsg::TOO_FEW_B)].first = true;
+	}
+	else if (Bcount > VALID_SHIP_NUM){
+		errMsgArr[int(ErrorMsg::TOO_MANY_B)].first = true;
+	}
+	//Check for adjacent
+	bool brk = false;
+	char* bottomPos;
+	char* rightPos;
+	for (int i = 0; i < gameBoard.R; ++i){
+		for (int j = 0; j < gameBoard.C; ++j){
+			//Check different to the bottom
+			if ((j != gameBoard.R - 1) && (gameBoard.matrix[i][j + 1] != gameBoard.matrix[i][j])){
+				bottomPos = std::find(std::begin(typeArr), std::end(typeArr), gameBoard.matrix[i][j + 1]);
+				if (bottomPos != std::end(typeArr)) { // fount adjacent
+					errMsgArr[int(ErrorMsg::AJACENT_ON_BOARD)].first = true;
+					brk = true;
+					break;
+				}
+			}
+			//Check different to the right
+			if ((i != gameBoard.C - 1) && (gameBoard.matrix[i + 1][j] != gameBoard.matrix[i][j])){
+				rightPos = std::find(std::begin(typeArr), std::end(typeArr), gameBoard.matrix[i + 1][j]);
+				if (rightPos != std::end(typeArr)) { // fount adjacent
+					errMsgArr[int(ErrorMsg::AJACENT_ON_BOARD)].first = true;
+					brk = true;
+					break;
+				}
+			}
+		}
+		if (brk) break;
+	}
+
+	//Check for error messages - print if needed
+	for (int i = 0; i < int(ErrorMsg::ERR_MGS_MAX); i++){
+		if (errMsgArr[i].first == true){
+			std::cout << errMsgArr[i].second << std::endl;
+		}
+	}
 }
 
-bool BattleshipGameManager::isValidShip(char type, int x, int y, const BattleBoard& gameBoard){
-	//TODO: save ship positions to a vector when done checking a ship (both valid or invalid)
-	//TODO: set errMsgArr[i].first=true if found error of type i
-	int size;
-	int i;
-	bool validRight = true;
-	bool validBottom = true;
+bool BattleshipGameManager::isValidShipRight(int x, int y, const BattleBoard& gameBoard){
+	char type = gameBoard.matrix[x][y];
+	int size = 0;
 
-	switch (gameBoard.matrix[x][y]){
+	while ((x < gameBoard.C-1) && (gameBoard.matrix[x][y] == type)){
+		size++;
+		//if same type above/bottom - return false
+		if ((y != 0) && (gameBoard.matrix[x][y - 1] == type) ||
+			(y != gameBoard.R-1) && (gameBoard.matrix[x][y + 1] == type)){
+			return false;
+		}
+		x++;
+	}
+	return (size == getSize(type));
+}
+
+bool BattleshipGameManager::isValidShipBottom(int x, int y, const BattleBoard& gameBoard){
+	char type = gameBoard.matrix[x][y];
+	int size = 0;
+
+	while ((y < gameBoard.R - 1) && (gameBoard.matrix[x][y] == type)){
+		size++;
+		//if same type left/right - return false
+		if ((x != 0) && (gameBoard.matrix[x-1][y] == type) ||
+			(x != gameBoard.C - 1) && (gameBoard.matrix[x+1][y] == type)){
+			return false;
+		}
+		y++;
+	}
+	return (size == getSize(type));
+}
+
+int BattleshipGameManager::getSize(char type){
+
+	switch (type){
 		case 'D':{
-					 size = int(shipSize::dSize);
+					 return int(shipSize::dSize);
 					 break;
 		}
 		case 'M':{
-					 size = int(shipSize::mSize);
+					 return int(shipSize::mSize);
 					 break;
 		}
 		case 'P':{
-					 size = int(shipSize::pSize);
+					 return int(shipSize::pSize);
 					 break;
 		}
 		case 'B':{
-					 size = int(shipSize::bSize);
+					 return int(shipSize::bSize);
 					 break;
 		}
 		case 'd':{
-					 size = int(shipSize::dSize);
+					 return int(shipSize::dSize);
 					 break;
 		}
 		case 'm':{
-					 size = int(shipSize::mSize);
+					 return int(shipSize::mSize);
 					 break;
 		}
 		case 'p':{
-					 size = int(shipSize::pSize);
+					 return int(shipSize::pSize);
 					 break;
 		}
 		case 'b':{
-					 size = int(shipSize::bSize);
+					 return int(shipSize::bSize);
 					 break;
 		}
 		default:{
-					size = 0;
+					return 0;
 					break;
 		}
 	}
-	//Check to the right - TODO: consider replacing this test with while loop + check if counter=size in the end.
-	for (i = 0; i < size; i++){
-		if (y + i > gameBoard.R){
-			validRight = false;
-			break;
-		}
-		else if (gameBoard.matrix[x][y + i] != gameBoard.matrix[x][y]){
-			validRight = false;
-			break;
-		}
-	}
-	if (validRight){//if found valid length, check that the next cell != your type
-		if (y + size < gameBoard.R){
-			if (gameBoard.matrix[x][y + size] == gameBoard.matrix[x][y]){
-				validRight = false;
-			}
-		}
-	}
+}
 
-	//check to the bottom - TODO: consider replacing this test with while loop + check if counter=size in the end.
-	for (i = 0; i < size; i++){
-		if (x + i > gameBoard.C){
-			validBottom = false;
-			break;
-		}
-		else if (gameBoard.matrix[x+i][y] != gameBoard.matrix[x][y]){
-			validBottom = false;
-			break;
-		}
+void BattleshipGameManager::updateErrMsgArrWrongSize(char type){
+	switch (type){
+	case 'D':{
+				 errMsgArr[int(ErrorMsg::WRONG_SIZE_SHAPE_D_A)].first = true;
+				 break;
 	}
-	if (validBottom){//if found valid length, check that the next cell != your type
-		if (x + size < gameBoard.C){
-			if (gameBoard.matrix[x + size][y] == gameBoard.matrix[x][y]){
-				validBottom = false;
-			}
-		}
+	case 'M':{
+				 errMsgArr[int(ErrorMsg::WRONG_SIZE_SHAPE_M_A)].first = true;
+				 break;
 	}
-
-	return ((validRight || validBottom) ? false : true);
+	case 'P':{
+				 errMsgArr[int(ErrorMsg::WRONG_SIZE_SHAPE_P_A)].first = true;
+				 break;
+	}
+	case 'B':{
+				 errMsgArr[int(ErrorMsg::WRONG_SIZE_SHAPE_B_A)].first = true;
+				 break;
+	}
+	case 'd':{
+				 errMsgArr[int(ErrorMsg::WRONG_SIZE_SHAPE_D_B)].first = true;
+				 break;
+	}
+	case 'm':{
+				 errMsgArr[int(ErrorMsg::WRONG_SIZE_SHAPE_M_B)].first = true;
+				 break;
+	}
+	case 'p':{
+				 errMsgArr[int(ErrorMsg::WRONG_SIZE_SHAPE_P_B)].first = true;
+				 break;
+	}
+	case 'b':{
+				 errMsgArr[int(ErrorMsg::WRONG_SIZE_SHAPE_B_B)].first = true;
+				 break;
+	}
+	default:{
+				break;
+	}
+	}
 }
