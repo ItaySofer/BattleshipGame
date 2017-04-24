2176ca092b7b12a5147ec69fc8523a5bdda73bfd
ItaysSofer <sofer.itay@gmail.com>
2017-04-24 20:48:37 +0300

    changed files listing according to new instructions

--- BattleshipGame/InputProcessor.cpp
+++ BattleshipGame/InputProcessor.cpp
@@ -54,63 +54,62 @@ InputProcessor::InputProcessor(int argc, char* argv[])
 
  bool InputProcessor::tryExtractFileNames()
  {
-	 std::string fileNamesFilePath = "fileNames.txt";
-	 std::stringstream command;
-	 command << "2>NUL dir /b /a-d \"" << folderPath << "\" > " << fileNamesFilePath;
-	 const std::string comandS = command.str();
-	 const char* comandC = comandS.c_str();
-	 if (system(comandC))
+	char buffer[4096];
+	std::string line;
+	std::stringstream command;
+	command << "2>NUL dir /b /a-d \"" << folderPath << "\"";
+	const std::string comandS = command.str();
+	const char* comandC = comandS.c_str();
+	FILE* fp = _popen(comandC, "r");
+	if (fp->_Placeholder == NULL)
+	{
+		std::stringstream testCommand;
+		testCommand << "2>NUL cd \"" << folderPath << "\"";
+		const std::string testCommandS = testCommand.str();
+		const char* testCommandC = testCommandS.c_str();
+		if (system(testCommandC))
+		{
+			std::string wrongPath = folderPath.empty() ? "Working Directory" : folderPath;
+			std::cout << "Wrong path: " << wrongPath << std::endl;
+			return false;
+		}
+	}
+
+	while (fgets(buffer, 4095, fp))
+	{
+	 line = std::string(buffer);
+	 StringUtils::replaceAll(line, "\r", "");
+	 StringUtils::replaceAll(line, "\n", "");
+
+	 std::stringstream filePath;
+
+	 if (StringUtils::endsWith(line, boardSuffix) && boardFilePath.empty())
 	 {
-		 std::stringstream testCommand;
-		 testCommand << "2>NUL cd \"" << folderPath << "\"";
-		 const std::string testCommandS = testCommand.str();
-		 const char* testCommandC = testCommandS.c_str();
-		 if (system(testCommandC))
-		 {
-			 std::string wrongPath = folderPath.empty() ? "Working Directory" : folderPath;
-			 std::cout << "Wrong path: " << wrongPath << std::endl;
-			 return false;
+		 filePath << concatenateAbsolutePath(folderPath, line);
+		 boardFilePath = filePath.str();
+	 }
+
+	 if (StringUtils::endsWith(line, dllSuffix)) {
+		 filePath << concatenateAbsolutePath(folderPath, line);
+		 if (dllFiles[0].empty()) {
+			 dllFiles[0] = filePath.str();
+		 }
+		 else if (dllFiles[1].empty()) {
+			 dllFiles[1] = filePath.str();
 		 }
 	 }
 
-	 std::ifstream fileNamesFile(fileNamesFilePath);
-	 std::string line;
-	 while (std::getline(fileNamesFile, line))
-	 {
-		 StringUtils::replaceAll(line, "\r", "");
-
-		 std::stringstream filePath;
-
-		 if (StringUtils::endsWith(line, boardSuffix) && boardFilePath.empty())
-		 {
-			 filePath << concatenateAbsolutePath(folderPath, line);
-			 boardFilePath = filePath.str();
+	 if (StringUtils::endsWith(line, attackSuffix)) {
+		 filePath << concatenateAbsolutePath(folderPath, line);
+		 if (attackFiles[0].empty()) {
+			 attackFiles[0] = filePath.str();
 		 }
-
-		 if (StringUtils::endsWith(line, dllSuffix)) {
-			 filePath << concatenateAbsolutePath(folderPath, line);
-			 if (dllFiles[0].empty()) {
-				 dllFiles[0] = filePath.str();
-			 }
-			 else if (dllFiles[1].empty()) {
-				 dllFiles[1] = filePath.str();
-			 }
-		 }
-
-		 if (StringUtils::endsWith(line, attackSuffix)) {
-			 filePath << concatenateAbsolutePath(folderPath, line);
-			 if (attackFiles[0].empty()) {
-				 attackFiles[0] = filePath.str();
-			 }
-			 else if (attackFiles[1].empty()) {
-				 attackFiles[1] = filePath.str();
-			 }
+		 else if (attackFiles[1].empty()) {
+			 attackFiles[1] = filePath.str();
 		 }
 	 }
-
-	 if (fileNamesFile.is_open()) {
-		 fileNamesFile.close();
-	 }
+	}
+	_pclose(fp);
 
 	 return true;
  }
